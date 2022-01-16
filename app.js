e547968bf373906aac7dc3a44f56dc831a71631d
Dimon <sardimka@gmail.com>
2022-01-16 19:47:43 +0400

    Initialised MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const config = require('config');
-
+const mongo = require('mongoose');
 const app = express();
-app.listen(config.get('port'), () => console.log('Started!'));
+
+const start = async () => {
+    try {
+        const db = await mongo.connect(config.get('db.mongoURI'), {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        });
+        app.listen(config.get('port'), () => console.log(`Started at ${config.get('port')}`));
+    } catch (error) {
+        console.log('Something happened on the server');
+        console.log(error.message);
+        process.exit();
+    }
+};
+
+start();
