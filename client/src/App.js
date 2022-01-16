30c0a7dc809e3b94e32695e2b68650d7b3419359
Dimon <sardimka@gmail.com>
2022-01-16 20:04:17 +0400

    Clean up the client project

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="">
+        <h3>Frontend is loaded now.</h3>
       </div>
     );
   }
-}
 
 export default App;
