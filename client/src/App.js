3721f1bc32f16f28283dfe8d4da1797930866062
Dimon <sardimka@gmail.com>
2022-01-21 18:30:17 +0400

    Added materialize and basic routing too

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import 'materialize-css';
+import useRoutes from './routes/routes';
 
 const App = () => {
+  const routes = useRoutes(false);
     return (
-      <div className="">
-        <h3>Frontend is loaded now.</h3>
-      </div>
+      <Router>
+        <div className="container">
+          <h3>Frontend is loaded now.</h3>
+          { routes }
+        </div>
+      </Router>
     );
   }
 
---
+++ client/src/screens/Links/Links.screen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Links = () => {
+    return(
+        <div>
+            <h1>TMP template Links</h1>
+        </div>
+    );
+}
+
+export default Links;
