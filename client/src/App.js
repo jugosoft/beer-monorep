57824f4eaa9468decbdb6d9cf2f38d9a54725d6f
Dimon <sardimka@gmail.com>
2022-01-21 18:33:35 +0400

    fix: renaming routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import 'materialize-css';
-import useRoutes from './routes/routes';
+import routes from './routes/routes';
 
 const App = () => {
-  const routes = useRoutes(false);
+  const routesRendered = routes(false);
     return (
       <Router>
         <div className="container">
           <h3>Frontend is loaded now.</h3>
-          { routes }
+          { routesRendered }
         </div>
       </Router>
     );
