417af8ff10c54877c2aac16b31bc3280ac9fcb09
AnnSinger <spivaka336@gmail.com>
2022-09-13 15:04:25 +0400

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   const config = app.get(ConfigService);
   const PORT = config.getOrThrow<number>(environment.port);
