8aac99443db22f37bacc1008ab66627e3c9113d9
dchernyh <sardimka@gmail.com>
2023-01-14 19:52:22 +0400

    jwt works are continued

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
-import { jwtConstants } from 'constants/auth';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
@@ -10,12 +9,19 @@ import { CryptoService } from './services/crypto.service';
 import { AtStrategy, RtStrategy } from './strategies';
 
 @Module({
-  imports: [
-    UsersModule, 
-    PassportModule,
-    JwtModule.register({}),
-  ],
-  providers: [AuthService, CryptoService, AtStrategy, RtStrategy],
-  controllers:[AuthController]
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.register({})
+    ],
+    providers: [
+        AuthService,
+        CryptoService,
+        AtStrategy,
+        RtStrategy
+    ],
+    controllers: [
+        AuthController
+    ]
 })
-export class AuthModule {}
+export class AuthModule { }
--- server/src/modules/auth/services/auth.service.ts
+++ server/src/modules/auth/services/auth.service.ts
@@ -1,88 +1,117 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 
-import { jwtConstants } from 'constants/auth';
-import { UserEntity } from 'src/entities/user.entity';
 import { CreateUserInput } from 'src/modules/users/inputs/create-user.input';
 import { UserService } from 'src/modules/users/services/user/user.service';
 import { Tokens } from '../types';
-import { CryptoService } from './crypto.service';
 import { atSecret, rtSecret } from "constants/crypt";
 import { AuthLoginInput } from '../inputs/auth-login.input';
 
+import * as argon2 from 'argon2';
+
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly usersService: UserService,
-    private readonly cryptoService: CryptoService,
-    private readonly jwtService: JwtService,
-    private readonly config: ConfigService) {}
+    constructor(
+        private readonly usersService: UserService,
+        private readonly jwtService: JwtService,
+        private readonly config: ConfigService
+    ) { }
 
-  async login(authLoginInput: AuthLoginInput): Promise<Tokens> {
-    const user = await this.usersService.getOneUserByName(authLoginInput.name);
+    async loginLocal(authLoginInput: AuthLoginInput): Promise<Tokens> {
+        const user = await this.usersService.getOneUserByName(authLoginInput.name);
+
+        if (!user) {
+            throw new BadRequestException('Access Denied. Try again!');
+        }
+
+        const passwordMatch = await argon2.verify(user.password, authLoginInput.password);
     
-    if (!user) throw new ForbiddenException('Access Denied. Try again!');
+        if (!passwordMatch) {
+            throw new BadRequestException('Access Denied. Try again!');
+        }
 
-    const passwordMatch = await this.cryptoService.verify(authLoginInput.password, user.password);
-    if (!passwordMatch) throw new ForbiddenException('Access Denied. Try again!');
+        const tokens = await this.getTokens(user.id, user.name);
+        
+        await this.updateRtHash(user.id, tokens.refreshToken);
 
-    const tokens = await this.getTokens(user.id, user.name);
-    await this.updateRtHash(user.id, tokens.refreshToken);
-    return tokens;
-  }
-
-  async registerLocal(createUserInput: CreateUserInput): Promise<Tokens> {
-    createUserInput.password = await this.cryptoService.hash(createUserInput.password);
-    const user = await this.usersService.createUser(createUserInput);
-    const tokens = await this.getTokens(user.id, user.name);
-    await this.updateRtHash(user.id, tokens.refreshToken);
-    return tokens;
-  }
-
-  async logout(userId: number) : Promise<boolean>{
-    await this.usersService.updateUserRt({id: userId, hashedRT: null});
-    return true;
-  }
-
-  async updateRtHash(userId: number, refreshToken: string): Promise<void> {
-    const hash = await this.cryptoService.hash(refreshToken);
-    await this.usersService.updateUserRt({id: userId, hashedRT: hash});
-  } 
-
-  async refreshTokens(userId: number, refreshToken: string): Promise<Tokens> {
-    const user = await this.usersService.getOneUser(userId);
-
-    if (!user || !user.hashedRT) throw new ForbiddenException('Access Denied. Try again!');
- 
-    if (!this.cryptoService.verify(refreshToken, user.hashedRT)) {
-      throw new ForbiddenException('Access Denied. Try again!');
+        return tokens;
     }
 
-    const tokens = await this.getTokens(user.id, user.name);
-    await this.updateRtHash(user.id, tokens.refreshToken);
-    return tokens;
-  }
+    async registerLocal(createUserInput: CreateUserInput): Promise<Tokens> {
+        const userExists = await this.usersService.getOneUserByName(createUserInput.name);
+        if (userExists) {
+            throw new BadRequestException('User already exists');
+        }
+        // Hash password
+        const hash = await this.hashData(createUserInput.password);
+        const newUser = await this.usersService.createUser({
+            ...createUserInput,
+            password: hash
+        });
 
-  async getTokens(userId: number, username: string): Promise<Tokens> {
-    const [accessToken, refreshToken] = await Promise.all([
-      this.signToken(userId, username, this.config.get<string>(atSecret), jwtConstants.halfOfHour),
-      this.signToken(userId, username, this.config.get<string>(rtSecret), jwtConstants.oneWeek),
-    ]);
+        const tokens = await this.getTokens(newUser.id, newUser.name);
 
-    return {
-      accessToken,
-      refreshToken,
-    };
-  }
+        await this.updateRtHash(newUser.id, tokens.refreshToken);
 
-  async signToken(userId: number, username: string, secret: string, duration: string): Promise<string> {
-    return await this.jwtService.signAsync({
-      sub: userId,
-      username, 
-    }, { 
-      expiresIn: duration,
-      secret: secret,
-    });
-  }
+        return tokens;
+    }
+
+    async logout(userId: number): Promise<boolean> {
+        await this.usersService.updateUserRt({ id: userId, hashedRT: null });
+        return true;
+    }
+
+    async updateRtHash(userId: number, refreshToken: string): Promise<void> {
+        const hashedRefreshToken = await this.hashData(refreshToken);
+        await this.usersService.updateUserRt({ id: userId, hashedRT: hashedRefreshToken });
+    }
+
+    async refreshTokens(userId: number, refreshToken: string): Promise<Tokens> {
+        const user = await this.usersService.getOneUser(userId);
+
+        if (!user || !user.hashedRT) throw new ForbiddenException('Access Denied. Try again!');
+
+        if (!this.cryptoService.verify(refreshToken, user.hashedRT)) {
+            throw new ForbiddenException('Access Denied. Try again!');
+        }
+
+        const tokens = await this.getTokens(user.id, user.name);
+        await this.updateRtHash(user.id, tokens.refreshToken);
+        return tokens;
+    }
+
+    async getTokens(userId: number, username: string): Promise<Tokens> {
+        const [accessToken, refreshToken] = await Promise.all([
+            this.jwtService.signAsync(
+                {
+                    sub: userId,
+                    username
+                },
+                {
+                    secret: this.config.get<string>(atSecret),
+                    expiresIn: '15m'
+                }
+            ),
+            this.jwtService.signAsync(
+                {
+                    sub: userId,
+                    username
+                },
+                {
+                    secret: this.config.get<string>(rtSecret),
+                    expiresIn: '7d'
+                }
+            )
+        ]);
+
+        return {
+            accessToken,
+            refreshToken
+        };
+    }
+
+    hashData(data: string) {
+        return argon2.hash(data);
+    }
 }
